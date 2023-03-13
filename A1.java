737b277a526397354dfc8d7cc490ef2bf712b16a
Tatincho <110904264+martinjueguen@users.noreply.github.com>
2023-03-13 16:03:57 -0300

    p

---
+++ A1.java
@@ -0,0 +1,18 @@
+package A1;
+import java.util.*;
+public class A1 {
+
+
+	
+	 
+	public static void main(String[] args) {
+		int num1;
+		System.out.println("Ingrese un numero");
+		Scanner teclado = new Scanner(System.in);
+		num1 = teclado.nextInt();
+		System.out.print("el numero ingresado es " + num1);
+			
+
+	}
+
+}
