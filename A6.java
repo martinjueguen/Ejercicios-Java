54ff18a4c7ddac1854f803ec02e1ed86b8960e9c
Tatincho <110904264+martinjueguen@users.noreply.github.com>
2023-03-20 15:09:48 -0300

    Add files via upload

---
+++ A6.java
@@ -0,0 +1,33 @@
+package Default;
+import java.util.Scanner;
+
+public class A5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num;
+		float pro=0;
+		float count=0;
+		int rta=1;
+		float des;
+		int en;
+		while(rta == 1) {
+			System.out.println("ingrese un numero");
+			num = sc.nextInt();
+			pro=num+pro;
+			count++;
+			System.out.println("quiere agregar otro numero? (1 para si)");
+			rta = sc.nextInt();
+			
+		}
+		en=(int) (pro/count);
+		des=pro/count;
+		System.out.println("la media es: "+en);
+		System.out.println("la media es: "+des);
+		
+		
+		
+	}
+
+}
