bee337b01f6eadcda57112e607df4c944a3aa9ab
Tatincho <110904264+martinjueguen@users.noreply.github.com>
2023-03-20 15:08:38 -0300

    Add files via upload

---
+++ A4.java
@@ -0,0 +1,17 @@
+package Default;
+
+public class A4 {
+
+	public static void main(String[] args) {
+		
+		
+		short num=-32768;
+		while (num<32768) {
+			System.out.println(num);
+			num++;
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
